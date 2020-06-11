307bffb194d4feddfc14675a76f6ada63ece284e
chenbingchang <940001047@qq.com>
2020-06-11 19:25:31 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,40 @@
 module.exports = {
   root: true,
   env: {
+    browser: true, // 自己加的
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: ["error", "single"], //强制使用单引号
+    semi: ["error", "never"], //强制不使用分号结尾
+    "comma-dangle": ["error", "always-multiline"],
+    // vue规则
+    // 多个属性，每个属性一行
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    // 多行属性对齐、缩进方式
+    "vue/html-indent": ["error", 2],
+    // 标签闭合括号
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "always"
+      }
+    ]
   }
-}
+};
---
+++ vue.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: './',
+  assetsDir: '',
+  css: {
+    loaderOptions: {
+      sass: {
+        /**
+         * 把prependData里面的资源全部分别加入到每个css文件里面
+         * 适合放scss的变量/函数/混合等不会生成css，从而避免在每个用的的地方@import引入
+         * 不适合放基础样式/全局样式，因为这样会在生成的每个css中都有基础或全局的样式
+         */
+        prependData: `
+          @import "@/common/css/common.scss";
+        `,
+      },
+    },
+  },
+  devServer: {
+    hot: true,
+    liveReload: true,
+    port: 1300,
+  },
+  chainWebpack: config => {
+    // 引入路劲的别名
+    config.resolve.alias
+      .set('@components', resolve('src/components'))
+      .set('@assets', resolve('src/assets'))
+      .set('@common', resolve('src/common'))
+    // html标题
+    config.plugin('html').tap(args => {
+      args[0].title = '冰封商城后台管理'
+      return args
+    })
+  },
+}
